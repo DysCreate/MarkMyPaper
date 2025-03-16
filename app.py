3434b11611181a024327b3b3abc3a2044a5abaf2
PARVANAMENON <parvanamenons@gmail.com>
2025-03-16 11:32:24 +0530

    loggin added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY']='thisisasecretkey'
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),nullable=False)
-    password = db.Column(db.String(80),nullable=False)
+    password =db.Column(db.String(80),nullable=False)
 
 
 @app.route('/')
