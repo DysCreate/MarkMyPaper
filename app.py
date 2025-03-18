5d9745ef5a1cbc0b524ee222eeee3b2c30791029
DysCreate <pranavpunnakkal305@gmail.com>
2025-03-18 16:25:15 +0530

    Initialised login

--- app.py
+++ app.py
@@ -1,29 +1,54 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
-app.config['SECRET_KEY']='thisisasecretkey'
+
+# Configurations should be set before initializing db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+db = SQLAlchemy(app)  # Initialize after setting config
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),nullable=False)
-    password =db.Column(db.String(80),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)  # Added unique=True for usernames
+    password = db.Column(db.String(80), nullable=False)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
+@app.route('/login', methods=['GET', 'POST'])  # Added methods to handle POST request
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html')
 
-def regiter():
+@app.route('/register')
+def register():
     return render_template('register.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
